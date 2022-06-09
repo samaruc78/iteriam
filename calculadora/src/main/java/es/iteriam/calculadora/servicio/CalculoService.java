5e995af6150ed8b01fc4aeb75c97bef55d54a995
samaruc78 <107059083+samaruc78@users.noreply.github.com>
2022-06-09 09:45:34 +0200

    Refactorizar a SOLID

--- calculadora/src/main/java/es/iteriam/calculadora/servicio/CalculoService.java
+++ calculadora/src/main/java/es/iteriam/calculadora/servicio/CalculoService.java
@@ -4,50 +4,29 @@
  *  
  * Implementacion de las funciones aritmeticas
  * 
+ * Por buenas practicas:
+ * no modificar en ampliaciones.
+ * Usar @MathOperationFactory
+ * 
  */
 
 package es.iteriam.calculadora.servicio;
 
-import es.iteriam.calculadora.models.MiModeloOperacion;
-import org.springframework.stereotype.Service;
 import java.math.BigDecimal;
+
+import org.springframework.stereotype.Service;
+
+import es.iteriam.calculadora.controllers.MathOperation;
+import es.iteriam.calculadora.controllers.MathOperationFactory;
+import es.iteriam.calculadora.models.MiModeloOperacion;
 
 @Service
 public class CalculoService {
 
-
-    public BigDecimal add (MiModeloOperacion model) {
-        return model.getA().add(model.getB());
+    public BigDecimal calcula(MiModeloOperacion model) {
+        MathOperationFactory operacionFactory = new MathOperationFactory();        
+        MathOperation operacion = operacionFactory.getOperation(model);        
+        return operacion.calculate();        
     }
-
-    public BigDecimal subtract(MiModeloOperacion model) {
-        return model.getA().subtract(model.getB());
-    }
-
-    public BigDecimal multiply(MiModeloOperacion model) {
-        return model.getA().multiply(model.getB());
-    }
-
-    public BigDecimal divide(MiModeloOperacion model) {
-
-        BigDecimal rstdo = new BigDecimal(0);
-
-        // Manejamos excepcion por si divisor es 0
-        try {
-            rstdo = model.getA().divide(model.getB());
-        } catch (ArithmeticException ex) {
-            ex.printStackTrace();
-            System.err.println(ex.getMessage());
-        }
-        return rstdo;
-    }
-
-    /**
-     * TO-DO function modulo. (a mod b)
-     * 
-     */
-    // public BigDecimal modulo(MiModeloOperacion model) {
-    //         No implementado 
-    // }
-
+    
 }
